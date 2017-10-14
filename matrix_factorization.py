0bf36fb8aa1cda0d667a18a275f5c649abaea933
clamli <boyizjuer2017@gmail.com>
2017-10-14 16:56:51 -0700

    change return value of function calculate matrix factorization

--- matrix_factorization.py
+++ matrix_factorization.py
@@ -1,39 +1,37 @@
 import numpy as np
 
 class MatrixFactorization:
-	def __init__(self, R, K, steps=5000, alpha=0.0002, beta=0.02, threshold=0.001):
-		self.R = R                                     # user-item matrix
+	def __init__(self, K, steps=5000, alpha=0.0002, beta=0.02, threshold=0.001):
+		# self.R = R                                     # user-item matrix
 		self.K = K                                     # feature number
 		self.steps = steps                             # iterate time
 		self.alpha = alpha                             # parameter1
 		self.beta = beta                               # parameter2
 		self.threshold = threshold                     # error threshold
+
+	def matrix_factorization(self, R):
 		self.P = np.random.rand(len(R), K)          # user latent factor
-		self.Q = np.random.rand(len(R[0]), K)       # item latent factor
-
-
-	def matrix_factorization(self):
-		self.Q = self.Q.T
+		self.Q = np.random.rand(len(R[0]), K).T       # item latent factor
 		for step in range(self.steps):
-			for i in range(len(self.R)):
-				for j in range(len(self.R[i])):
-					if self.R[i][j] > 0:
-						eij = self.R[i][j] - np.dot(self.P[i, :], self.Q[:, j])
+			for i in range(len(R)):
+				for j in range(len(R[i])):
+					if R[i][j] > 0:
+						eij = R[i][j] - np.dot(self.P[i, :], self.Q[:, j])
 						for k in range(self.K):
 							self.P[i][k] = self.P[i][k] + self.alpha * (2 * eij * self.Q[k][j] - self.beta * self.P[i][k])
 							self.Q[k][j] = self.Q[k][j] + self.alpha * (2 * eij * self.P[i][k] - self.beta * self.Q[k][j])
 			pR = np.dot(self.P, self.Q)
 			e = 0
-			for i in range(len(self.R)):
-				for j in range(len(self.R[i])):
-					if self.R[i][j] > 0:
-						e = e + pow(self.R[i][j] - pR[i][j], 2)
+			for i in range(len(R)):
+				for j in range(len(R[i])):
+					if R[i][j] > 0:
+						e = e + pow(R[i][j] - pR[i][j], 2)
 						for k in range(self.K):       # add regularization
 							e = e + (self.beta/2) * (pow(self.P[i][k], 2) + pow(self.Q[k][j], 2))
 			if e < self.threshold:
 				break
 		self.Q = self.Q.T
-		return self.P, self.Q, np.dot(self.P, self.Q.T), e
+		return np.dot(self.P, self.Q.T)
 
 
 	def calculate_average_RMSE(self, oRate, pRate, start, end):
