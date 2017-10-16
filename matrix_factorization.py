add1f52bac10ebaf89ff1b519d5347769329c752
clamli <boyizjuer2017@gmail.com>
2017-10-15 17:48:39 -0700

    add matrix_factorization_by_NMF

--- matrix_factorization.py
+++ matrix_factorization.py
@@ -91,8 +91,8 @@ class MatrixFactorization:
             cnt = 0
             for i,j in zip(row,col):
                 e = e + pow(R[i,j] - pR[i,j], 2)
-                for k in range(self.K):       # add regularization
-                    e = e + (self.beta/2) * (pow(self.P[j][k], 2) + pow(self.Q[i][k], 2))
+                # for k in range(self.K):       # add regularization
+                #     e = e + (self.beta/2) * (pow(self.P[j][k], 2) + pow(self.Q[i][k], 2))
                 cnt = cnt + 1
             e = math.sqrt(e/cnt)
             error_list.append(e)
@@ -111,6 +111,52 @@ class MatrixFactorization:
 
         return np.dot(self.Q, self.P.T)
 
+
+    def approximate_matrix_factorization_by_NMF(self, R):
+        self.P = np.random.rand(R.shape[1], self.K)         # user latent factor
+        self.Q = np.random.rand(R.shape[0], self.K)         # item latent factor 
+        print("MF iterations")
+        error_list = []
+        for step in range(self.steps):
+            print(step)
+            x = sparse.find(R)
+            row = x[0]                 # item
+            col = x[1]                 # user
+
+            #### User Part ####
+            denominatorV = np.dot(self.Q.T, self.Q, self.P.T).T + 1e-9   # u*k
+            self.P = self.P * (R.T*self.Q) / denominatorV
+
+            #### Item Part ####
+            denominatorU = np.dot(self.P.T, self.P, self.Q.T).T + 1e-9   # i*k
+            self.Q = self.Q * (R*self.P) / denominatorU
+
+            #### Calculate Predicted Matrix ####
+            pR = np.dot(self.Q, self.P.T)
+            e = 0
+            cnt = 0
+            for i,j in zip(row,col):
+                e = e + pow(R[i,j] - pR[i,j], 2)
+                # for k in range(self.K):       # add regularization
+                #     e = e + (self.beta/2) * (pow(self.P[j][k], 2) + pow(self.Q[i][k], 2))
+                cnt = cnt + 1
+            e = math.sqrt(e/cnt)
+            error_list.append(e)
+
+            #### Judge if small than threshold ###
+            if e < self.threshold:
+                break
+
+        #### Plot RMSE picture ####
+        plt.figure(1) # 创建图表1
+        plt.title('RMSE for each iteration')
+        plt.xlabel('Iteration') 
+        plt.ylabel('RMSE value')
+        plt.plot(error_list)
+        plt.show()
+
+        return np.dot(self.Q, self.P.T)
+        
 
     def calculate_average_RMSE(self, oRate, pRate, start, end):
         user_num = oRate.shape[1]
